eb3887dd321c18a0c1ff0b18792604302265bbdf
TQ155 <tariq1992remastered@gmail.com>
2023-10-27 16:54:18 +0100

    First Commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+# Accessing the Static Website Using 
+
+# 1- Access the website via website-endpoint : my-7949882270-bucket.s3-website-us-east-1.amazonaws.com	
+# 2- CloudFront Domain Name: https://d1orpwy4d7zciz.cloudfront.net
+# 3- Access the bucket object via its S3 object URL : https://my-7949882270-bucket.s3.amazonaws.com/index.html 
---
+++ commands.py
@@ -0,0 +1,11 @@
+# Shift + Ctrl + P 
+# Create an instance : aws ec2-instance-connect send-ssh-public-key \
+# Create an EBS : aws ec2 create-volume 
+# describe volumes Command 
+# aws ec2 create-vpc 
+# create RDS 
+# create DynamoDB
+# aws iam create-policy --policy-name < name goes here> --policy-document file://< file name goes here>
+# aws iam attach-user-policy --policy-arn arn:aws:iam:ACCOUNT-ID:aws:policy/AdministratorAccess --user-name <  name goes here>
+
+ 
